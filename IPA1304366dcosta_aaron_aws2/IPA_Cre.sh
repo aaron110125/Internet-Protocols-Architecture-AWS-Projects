eb935329f6e2ffcc3076f012b173d8e2d53aeb2f
Aaron D'Costa <dcostaaaron@yahoo.in>
2021-01-29 08:53:17 -0500

    Internet and Protocols Architecture AWS Projects

---
+++ IPA1304366dcosta_aaron_aws2/IPA_Cre.sh
@@ -0,0 +1,3 @@
+aws cloudformation create-stack --stack-name IPA-VPC1 --template-body file://script2.json --parameters ParameterKey=myVPC,ParameterValue="IPA-VPC" ParameterKey=myPublicSubnet1,ParameterValue="PublicSubnet1" ParameterKey=myPublicSubnet2,ParameterValue="PublicSubnet2" ParameterKey=myPrivateSubnet1,ParameterValue="PrivateSubnet1" ParameterKey=myPrivateSubnet2,ParameterValue="PrivateSubnet2" ParameterKey=myInternetGateway,ParameterValue="IPA-IGW" ParameterKey=myPublicRouteTable,ParameterValue="IPA-PublicRouteTables" ParameterKey=myPrivateRouteTable,ParameterValue="IPA-PrivateRouteTable" ParameterKey=myNatGateway,ParameterValue="IPA-NATG"
+
+
